169d558090567ed8458d4bff96fc656ed4000fd7
Bruno <bru14112@gmail.com>
2026-04-26 05:05:20 -0300

    Primeira versao do projeto monitor Magalu

---
+++ monitor.js
@@ -0,0 +1,6 @@
+const axios = require ('axios');
+const cheerio = require ('cheerio');
+console.log ("O Projeto TrackGo ! foi iniciado com sucesso !");
+async function verificarPreco (produtoUrl, PrecoAlvo)
+{const {data} =await axios.get(produtoUrl, {headers: {'user-Agent': 'Mozilla/5.0}});
+const $ = cheerio .load(data)
